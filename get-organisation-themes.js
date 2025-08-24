95e1f2416e43bc17e5f2f5cc87acfac6392caa56
Ricky Mulderij <rickmulderij@hotmail.com>
2025-08-24 09:58:54 +0200

    nieuwe fixes

--- get-organisation-themes.js
+++ get-organisation-themes.js
@@ -55,17 +55,7 @@ router.get('/:orgId', async (req, res) => {
       const existingInsight = existingInsights?.find(insight => insight.theme_id === theme.id)
 
       const totalEmployees = employees?.length || 0
-      
-      // Haal werkelijke voortgang op uit gesprekresultaten
-      const { data: results, error: resultsError } = await supabase
-        .from('gesprekresultaten')
-        .select('werknemer_id')
-        .eq('werkgever_id', orgId)
-        .eq('theme_id', theme.id)
-
-      if (resultsError) throw resultsError
-      
-      const completedEmployees = results?.length || 0
+      const completedEmployees = existingInsight?.voltooide_medewerkers || 0
       const averageScore = existingInsight?.gemiddelde_score || null
 
       // Bepaal samenvatting status
